d979e13156d0eb8f5854b14b63de2fc0601bec7b
Jamshidbek129 <shokirovjamshidbek389@gmail.com>
2025-05-12 00:24:48 +0500

    Model qo'shildi

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastai.vision.all import *
+import streamlit as st
+import pathlib
+import plotly.express as px
+import matplotlib.pyplot as plt
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+st.text("Assalomu aleykum")
+st.title("Daraxtlar va Gullar")
+
+fayl=st.file_uploader("Rasm yuklash",  type=['png', 'gif', 'svg', 'jpeg'])
+if fayl:
+  rasm=PILImage.create(fayl)
+
+  model=load_learner('C:\TATU_darslar\Mobil ilovalar\Daraxtlar\model (1).pkl')
+
+  bashorat, id, ehtimollik=model.predict(rasm)
+
+  st.image(fayl)
+  st.success(f"Bashorat: {bashorat}")
+  st.info(f"Ehtimollik: {ehtimollik[id]*100:.1f}%")
+
+  fig=px.bar(y=ehtimollik*100, x=model.dls.vocab)
+  st.plotly_chart(fig)
